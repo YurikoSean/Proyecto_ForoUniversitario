45096a858b6cb26d58ee9fc28d20df9a11885ee6
Yuriko <yurikosean@gmail.com>
2025-03-10 19:12:04 +0100

    se actualizó el controlador, previo a la vista.

--- controladores/controlador.php
+++ controladores/controlador.php
@@ -15,14 +15,57 @@ class Controlador
         $controlador="inicio";
         $accion="";
 
-        if (isset($_GET["controlador"]) and !empty($_GET["controlador"]))
+        if (isset($_GET["controllador"]) and !empty($_GET["controlador"]))
         {
-            $controlador= $_GET["controlador"];
+            $controlador= $_GET["controllador"];
         }
         if (isset($_GET["accion"]) and !empty($_GET["accion"]))
         {
             $accion= $_GET["accion"];
         }
+
+        if (file_exists("./controladores/".ucfirst($controlador)."controlador.php")) 
+        {
+          echo "Existe controlador";
+        }   
+        else {
+            echo "No existe controlador";
+        }
+
+        /*
+        require_once("./controladores/".ucfirst($controlador)."controlador.php");
+        $nombreClase=ucfirst($controlador)."controlador";
+        if($accion!="")
+        {
+            if (method_exists($contro,$accion))
+            {
+                $contro->accion();
+            }
+            else {
+                $data["errorvalidacion"]="Imposible realizar la acción";
+                require_once("./vistas/vista.php");
+                $vista= new vista();
+                $vista->render("inicio",$data);
+            }
+        }
+        else 
+        {
+            require_once("./controladores/inicioControlador.php");
+            $contro=new inicioControlador();
+            $contro->irAInicio();
+        }
+
+        else
+        {
+            require_once("./controladores/inicioControlador.php");
+            $contro=new inicioControlador();
+            $contro->irAInicio();
+        }
+
+        */
+
+
+
         echo $controlador . " - " . $accion;
         echo "llamo a main";
 
