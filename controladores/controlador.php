d847e06b1802f6e679c146093d437b5805d89728
Yuriko <yurikosean@gmail.com>
2025-03-10 18:52:46 +0100

    se modificó controlador.php para las pruebas del htaccess

--- controladores/controlador.php
+++ controladores/controlador.php
@@ -1,14 +1,34 @@
 <?php
 
-class Controlador {
+class Controlador 
+{
 
+    private $controlador;
+    private $accion;
     public function _construct(Type $var = null) {
         $this->var = $var;
     }
 
     public function main()
     {
+        
+        $controlador="inicio";
+        $accion="";
+
+        if (isset($_GET["controlador"]) and !empty($_GET["controlador"]))
+        {
+            $controlador= $_GET["controlador"];
+        }
+        if (isset($_GET["accion"]) and !empty($_GET["accion"]))
+        {
+            $accion= $_GET["accion"];
+        }
+        echo $controlador . " - " . $accion;
         echo "llamo a main";
+
+        echo "<pre>";
+        print_r($_GET);
+        echo "</pre>";
     }
 
 }
