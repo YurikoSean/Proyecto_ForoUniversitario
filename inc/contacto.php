59c3dd1bd7b05c47907755fb09842c7636e36c60
Yuriko <yurikosean@gmail.com>
2025-03-18 17:10:14 +0100

    rutas

--- inc/contacto.php
+++ inc/contacto.php
@@ -6,7 +6,7 @@
     <title>Contacto</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="assets/css/estilos.css">
+    <link rel="stylesheet" href="../assets/css/estilos.css">
 </head>
 <body>
 
@@ -15,7 +15,7 @@
         <div class="container p-5">
             <div class="row">
                 <div class="col">
-                        <img class="mb-4" src="../img/robot_foro.png" alt="img logo" width="150" height="100">
+                        <img class="mb-4" src="../../../img/robot_foro.png" alt="img logo" width="150" height="100">
                     <h1 class="h3 mb-3 fw-normal h1-new-user">Contacto</h1>
                 </div>
             </div>       
--- inc/footer.php
+++ inc/footer.php
@@ -3,8 +3,8 @@
 <footer class="py-3 my-4 text-center mt-auto">
     <ul class="nav justify-content-center border-bottom pb-3 mb-3">
       <li class="nav-item"><a href="../../../index.php" class="nav-link px-2 text-body-secondary">Inicio</a></li>
-      <li class="nav-item"><a href="terminos.html" class="nav-link px-2 text-body-secondary">Term. & Cond.</a></li>
-      <li class="nav-item"><a href="contacto.php" class="nav-link px-2 text-body-secondary">Contacto</a></li>
+      <li class="nav-item"><a href="../../inc/terminos.html" class="nav-link px-2 text-body-secondary">Term. & Cond.</a></li>
+      <li class="nav-item"><a href="../../inc/contacto.php" class="nav-link px-2 text-body-secondary">Contacto</a></li>
     </ul>
     <p class="text-center text-body-secondary text-footer">&copy; 2025 Foro Universitario, Group 19</p>
     <!-- Validación de CSS -->
