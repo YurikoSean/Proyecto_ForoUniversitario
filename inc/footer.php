89e3ae8fffcb2c00338bfe9f4d1283bf85fbbbaf
Yuriko <yurikosean@gmail.com>
2025-03-18 17:05:06 +0100

    actualizacion

--- inc/footer.php
+++ inc/footer.php
@@ -1,3 +1,18 @@
-    <!--FOOTER-->
-    
-
+<!--FOOTER-->
+<div class="b-example-divider"></div>
+<footer class="py-3 my-4 text-center mt-auto">
+    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+      <li class="nav-item"><a href="../../../index.php" class="nav-link px-2 text-body-secondary">Inicio</a></li>
+      <li class="nav-item"><a href="terminos.html" class="nav-link px-2 text-body-secondary">Term. & Cond.</a></li>
+      <li class="nav-item"><a href="contacto.php" class="nav-link px-2 text-body-secondary">Contacto</a></li>
+    </ul>
+    <p class="text-center text-body-secondary text-footer">&copy; 2025 Foro Universitario, Group 19</p>
+    <!-- Validación de CSS -->
+    <p>
+      <a href="http://jigsaw.w3.org/css-validator/check/referer">
+        <img style="border:0;width:88px;height:31px"
+             src="http://jigsaw.w3.org/css-validator/images/vcss"
+             alt="¡CSS Válido!" />
+      </a>
+    </p>
+</footer>
