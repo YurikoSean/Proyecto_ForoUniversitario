abbf49b96dfc3fc1be615376b386dc3d318eb578
Yuriko <yurikosean@gmail.com>
2025-03-22 19:29:26 +0100

    Creación BD
    *Carlos Lanza

--- bd/foro_universitario.sql
+++ bd/foro_universitario.sql
@@ -1,1 +1,14 @@
-BASE DE DATOS
+/*BASE DE DATOS*/
+CREATE DATABASE IF NOT EXISTS forouniversitario;
+USE forouniversitario;
+
+-- Tabla de usuarios
+CREATE TABLE usuario (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    apellidos VARCHAR(100) NOT NULL,
+    fecha_nacimiento DATE NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    nick VARCHAR(50) NOT NULL UNIQUE,
+    contrasena VARCHAR(255) NOT NULL
+);
